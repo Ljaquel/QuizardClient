08858567ca60d6e76772e746af511325c204d5e6
Bryan Chavez <bryan.chavez@stonybrook.edu>
2021-10-22 11:50:11 -0400

    deleting notes from Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import QuizCard from '../components/QuizCard';
 import { AuthContext } from '../context/auth';
 import { gql, useQuery, useMutation } from '@apollo/client';
