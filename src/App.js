b07519af37608e03758022df7255f6432c9503f6
Liomard J Mesa <jaquelmesa@gmail.com>
2021-10-26 19:25:50 -0400

    Temporary Tab Icon Added

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import './App.css';
 function App() {
   return (
     <AuthProvider>
-      <Navbar/>
       <Router>
+        <Navbar/>
         <Switch>
           <Route exact path='/' component={Home}/>
           <AuthRoute exact path='/login' component={Login}/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,36 @@
 import React, { useContext } from 'react'
 import { AuthContext } from '../context/auth'
-import { Link, BrowserRouter as Router } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   const { user, logout } = useContext(AuthContext)
   return (
-    <Router>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="/" exact={true} >{user ? user.username : "Home"}</NavLink>
+            </li>
+          </ul>
+          {!user ?
+            <ul className="navbar-nav">
               <li className="nav-item">
-                <Link className="navbar-brand" to="/">{user ? user.username : "Home"}</Link>
+                <NavLink className="nav-link" activeClassName="active" to="/login">Login</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" activeClassName="active" to="/register">Register</NavLink>
               </li>
             </ul>
-            {!user ?
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <Link className="nav-link active" to="/login">Login</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link active" to="/register">Register</Link>
-                </li>
-              </ul>
-            :
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <button className="btn btn-secondary nav-link active" style={{cursor:"pointer"}} onClick={logout}>Logout</button>
-                </li>
-              </ul>
-            }
-          </div>
+          :
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <button className="btn btn-secondary nav-link" style={{cursor:"pointer"}} onClick={logout}>Logout</button>
+              </li>
+            </ul>
+          }
         </div>
-      </nav>
-    </Router>
+      </div>
+    </nav>
   )
 }
