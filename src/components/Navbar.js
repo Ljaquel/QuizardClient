ea051ed4e2ffd2d1b8e9f4b68885c27d538891d4
Liomard J Mesa <jaquelmesa@gmail.com>
2021-11-03 17:31:12 -0400

    Search Screen Input moved to SearchScreen.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,9 @@
-import React, { useContext,useState } from 'react'
+import React, { useContext } from 'react'
 import { AuthContext } from '../context/auth'
 import { NavLink, useHistory } from "react-router-dom"
 import { Dropdown } from 'react-bootstrap'
-import '../styles/Navbar.css'
 
-const Navbar=(props) =>{
-  const [values, setValues] = useState({
-    userInput: "", 
-    error: null,
-  });
-
-  const onChange = (e) => {
-    if (values.error) {
-      setValues({ ...values, [e.target.name]: e.target.value, error: null });
-    } else {
-      setValues({ ...values, [e.target.name]: e.target.value });
-    }
-  };
-  const SearchbtClicked=()=>{ 
-    if (!values.userInput==''){
-      console.log(values.userInput)
-      history.push('/searchscreen')
-    }
-    else
-      console.log("Empty userInput") 
-  };
-
+const Navbar=() =>{
   const { user, logout } = useContext(AuthContext)
   const history = useHistory();
 
@@ -33,29 +11,14 @@ const Navbar=(props) =>{
     <nav className="navbar navbar-expand-lg navbar-dark nav-container p-1 bg-dark">
       <div className="container-fluid px-2">
         <div className="collapse navbar-collapse">
-          {user&& 
-            <ul className="navbar-nav mb-2 mb-lg-0"  >
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/" exact={true} >Home</NavLink>
-              </li>
-            </ul>
-          } 
-          {user &&
-          <ul className="texField">
-            <input 
-                required
-                type="text"
-                placeholder="Quiz Name" 
-                name="userInput"
-                value={values.userInput}
-                onChange={onChange} 
-              /> 
-            <button onClick={SearchbtClicked}>Search</button> 
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/" exact={true} >Home</NavLink>
+            </li>
           </ul>
-          } 
           {!user
           ?
-            <ul className="navbar-nav loginRegisterBtns">
+            <ul className="navbar-nav">
               <li className="nav-item">
                 <NavLink className="nav-link" activeClassName="active" to="/login">Login</NavLink>
               </li>
@@ -64,16 +27,23 @@ const Navbar=(props) =>{
               </li>
             </ul>
           :
-            <Dropdown className="myclass">
-              <Dropdown.Toggle variant="success" size={"sm"} id="dropdown-basic">
-                {user.username[0].toUpperCase()}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item onClick={() => history.push('/profile')}>Profile</Dropdown.Item>
-                <Dropdown.Item onClick={() => history.push('/settings')}>Settings</Dropdown.Item> 
-                <Dropdown.Item onClick={logout} className="text-dark">Logout</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
+            <>
+              <ul className="navbar-nav">
+                <li className="nav-item me-3">
+                  <NavLink className="nav-link" activeClassName="active" to="/searchscreen">Search</NavLink>
+                </li>
+              </ul>
+              <Dropdown className="myclass">
+                <Dropdown.Toggle variant="success" size={"sm"} id="dropdown-basic">
+                  {user.username[0].toUpperCase()}
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={() => history.push('/profile')}>Profile</Dropdown.Item>
+                  <Dropdown.Item onClick={() => history.push('/settings')}>Settings</Dropdown.Item> 
+                  <Dropdown.Item onClick={logout} className="text-dark">Logout</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </>
           } 
         </div>
       </div>
--- src/pages/SearchScreen.js
+++ src/pages/SearchScreen.js
@@ -1,5 +1,4 @@
-import React, { useContext, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react'
 import { useQuery } from '@apollo/client';
 
 import QuizCard from '../components/QuizCard';
@@ -9,6 +8,8 @@ import { FETCH_QUIZZES_QUERY } from '../Calls';
 
 const QuizScreen = () => {
   const { user } = useContext(AuthContext);
+  const [ searchInput, setSearchInput ] = useState("")
+
   const { data, refetch } = useQuery(FETCH_QUIZZES_QUERY);
 
   useEffect(() => {
@@ -16,16 +17,23 @@ const QuizScreen = () => {
   }, [refetch]);
 
   if(!data) { return <Loading/> }
-  const { getQuizzes: quizzes } = data;
+  let { getQuizzes: quizzes } = data;
+
+  if(quizzes && searchInput !== "") quizzes = quizzes.filter(q => q.name.toLowerCase().includes(searchInput));
  
   return (
     <div className="container">
-      <div className="row">
-        <h1 className="mt-3 mb-4 col-3">Search</h1>
+      <div className="row mt-3 mb-4 py-2">
+        <div className="col-6 offset-3">
+          <div className="input-group">
+            <span className="input-group-text">Search</span>
+            <input type="text" className="form-control" placeholder="Type quiz name" value={searchInput} onChange={(e) => setSearchInput(e.target.value.toLowerCase())}/>
+          </div>
+        </div>
       </div>
       <div className="row row-cols-auto g-3"> 
-        {user && quizzes && quizzes.map((quiz, index) =>
-          <div className="col"  key={index}>  
+        {user && quizzes && searchInput !== "" && quizzes.map((quiz, index) =>
+          <div className="col"  key={index}>
             <QuizCard quiz={quiz} home={true}/>
           </div>
         )}
