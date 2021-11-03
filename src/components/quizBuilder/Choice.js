8564870261159ca719371b7c75bd1e893ac629f8
Liomard J Mesa <jaquelmesa@gmail.com>
2021-11-02 23:51:06 -0400

    QuizBuilder Updates

--- src/components/quizBuilder/Choice.js
+++ src/components/quizBuilder/Choice.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const Choice = ({index, choice, isAnswer, updateChoice, updateAnswer}) => {
+const Choice = ({index, choice, isAnswer, updateChoice, updateAnswer, deleteChoice}) => {
   return (
-    <div className="row m-0 p-0 px-0 mb-1">
+    <div className="row m-0 p-0 px-0 py-2 my-1 align-items-center border border-1 border-secondary rounded">
+      <div className="col col-auto pe-3 m-0">=</div>
       <div className="col col-auto p-0 m-0 align-self-center">
         <input className="form-check-input" type="radio" name="flexRadioDefault" defaultChecked={isAnswer} onClick={() => updateAnswer(index)}></input>
       </div>
@@ -15,6 +16,7 @@ const Choice = ({index, choice, isAnswer, updateChoice, updateAnswer}) => {
           onChange={(e) => updateChoice(index, e.target.value)}
           />
       </div>
+      <div className="col col-auto pe-3 m-0 cursor-pointer" onClick={() => deleteChoice(index)}>x</div>
     </div>
   )
 }
