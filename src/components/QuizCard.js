12335a8251f3e2d8fb6c8608acdba28ea0a47bb6
Bryan Chavez <bryan.chavez@stonybrook.edu>
2021-11-05 00:13:43 -0400

    Quizcard Changed, SearchScreen Change

--- src/components/QuizCard.js
+++ src/components/QuizCard.js
@@ -1,24 +1,60 @@
 import React, {useContext} from 'react';
+import { useHistory } from 'react-router';
 import moment from 'moment';
 import { AuthContext } from '../context/auth';
 import { Link } from "react-router-dom";
-import '../styles/QuizCard.css'
+import '../styles/QuizCard.css' 
 
 function QuizCard(props) {
   const { quiz } = props;
   const { user } = useContext(AuthContext);
   const destination = quiz.published ? "/quizscreen/" : user?._id === quiz.creator ? "/quizbuilder/" :  "/quizscreen/"
+  const history= useHistory();
 
   return (
-    <div className="card" style={{width: "15rem"}}>
-      <div className="card-body">
-        <Link to={`${destination}${quiz._id}`} className="card-subtitle text-muted">{quiz.name}</Link>
-        <p className="card-text" style={{fontSize: "13px"}}>{quiz.description.length < 70 ? quiz.description : "Description..."}</p>
-        <h6 className="card-subtitle text-muted mt-0">
-          {props.home && quiz?.creator === user?._id && "Owner - "}{quiz.publishedDate.length>0?"Published "+moment(quiz.publishedDate).fromNow():"Created "+moment(quiz.createdAt).fromNow()}
-        </h6>
-      </div>
-    </div>
+    <div> 
+      {history.location.pathname ==="/" &&
+        <Link to={`${destination}${quiz._id}`} className="searchScreencardDiv card-subtitle text-muted" >
+        <div style={{width: "15rem",height:"134px", border:"solid", padding:"16px",marginBottom:"10px"}}>
+            
+              {quiz.name} 
+              <p className="card-text" style={{fontSize: "13px"}}>{quiz.description.length < 70 ? quiz.description : "Description..."}</p>
+              <h6 className="card-subtitle mt-0">
+                {props.home && quiz?.creator === user?._id && "Owner - "}{quiz.publishedDate.length>0?"Published "+moment(quiz.publishedDate).fromNow():"Created "+moment(quiz.createdAt).fromNow()}
+              </h6>
+          
+        </div>
+      </Link> 
+      }
+      {history.location.pathname ==="/searchscreen" &&
+      <Link to={`${destination}${quiz._id}`} className="searchScreencardDiv card-subtitle text-muted" >
+        <div style={{width: "100%", border:"solid", padding:"16px",marginBottom:"10px"}}>
+            
+              {quiz.name} 
+              <p className="card-text" style={{fontSize: "13px"}}>{quiz.description.length < 140 ? quiz.description : "Description..."}</p>
+              <h6 className="card-subtitle mt-0">
+                {props.home && quiz?.creator === user?._id && "Owner - "}{quiz.publishedDate.length>0?"Published "+moment(quiz.publishedDate).fromNow():"Created "+moment(quiz.createdAt).fromNow()}
+              </h6>
+
+        </div>
+      </Link> 
+      }
+
+      {history.location.pathname ==="/profile" &&
+        <Link to={`${destination}${quiz._id}`} className="searchScreencardDiv card-subtitle text-muted" >
+        <div style={{width: "100%", border:"solid", padding:"16px",marginBottom:"10px"}}>
+            
+              {quiz.name} 
+              <p className="card-text" style={{fontSize: "13px"}}>{quiz.description.length < 70 ? quiz.description : "Description..."}</p>
+              <h6 className="card-subtitle mt-0">
+                {props.home && quiz?.creator === user?._id && "Owner - "}{quiz.publishedDate.length>0?"Published "+moment(quiz.publishedDate).fromNow():"Created "+moment(quiz.createdAt).fromNow()}
+              </h6>
+            
+        </div>
+      </Link> 
+      }
+
+    </div> 
   )
 }
 
