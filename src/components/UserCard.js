35574113473d07164b045debf0adfa533d8707ff
Justin Fagan <jbfagan7@gmail.com>
2021-12-03 10:20:22 -0500

    Started the UserCard component

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,22 +1,63 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/auth';
 
-function UserCard(props) {
-  const { currentUser } = props;
-  const destination =  "/profile/"
-
+function UserCard({ home, user }) {
+  const history = useHistory()
+  const { contextUserId } = useContext(AuthContext);
   return (
-    <div> 
-      <Link to={`${destination}${currentUser?._id}`} className="searchScreencardDiv card-subtitle text-muted" >
-        <div className="searchScreencardDiv card-subtitle text-muted" >
-          <div style={{width: "15rem",height:"134px", border:"solid", padding:"16px",marginBottom:"10px"}}>                
-            <h5>Name: {currentUser?.name}  </h5>                
-            <p className="card-text" style={{fontSize: "13px"}}>Username:{currentUser?.username}</p>
+      <div
+        className="card mb-3 quiz-card rounded h-100"
+        style={{ maxWidth: "400px" }}
+        onClick={() => history.push(`/profile/${user._id}`)}
+      >
+        <div className="row g-0 h-100">
+          <div className="col-5 px-1 py-1 h-100">
+            <img
+              className="img-thumbnail"
+              src={
+                user?.avatar?.url
+                  ? user?.avatar?.url
+                  : "https://res.cloudinary.com/ljaquel/image/upload/v1637970039/admin/imagePlaceholder_fxpfme.png"
+              }
+              style={{ padding: "0.10rem", backgroundColor: "#fff" }}
+              alt="..."
+            />
+          </div>
+          <div className="col-7 border-start h-100">
+            <div className="card-body">
+              <h5 className="card-title">{user?.name}</h5>
+              <p className="card-text">
+                {.description.length < 80
+                  ? quiz.description
+                  : quiz.description.substring(0, 80) + "..."}
+              </p>
+            </div>
           </div>
         </div>
-      </Link>  
-    </div> 
-  )
+        <div className="card-footer border-start">
+          <div className="row">
+            <div className="col align-self-center">
+              <small className="text-muted">
+                {quiz.publishedDate.length > 0
+                  ? "Published " + moment(quiz.publishedDate).fromNow()
+                  : "Created " + moment(quiz.createdAt).fromNow()}
+              </small>
+            </div>
+            <div className="col-auto align-self-center pt-1">
+              <Rating
+                name="readOnly"
+                value={quiz.rating}
+                size="small"
+                precision={0.5}
+                readOnly
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+  );
 }
 
 export default UserCard;
