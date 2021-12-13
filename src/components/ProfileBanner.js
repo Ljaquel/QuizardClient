3c4e37960c22af6759451e9adb5148980ccd08fa
Liomard J Mesa <jaquelmesa@gmail.com>
2021-12-13 02:31:06 -0500

    Update ProfileBanner.js

--- src/components/ProfileBanner.js
+++ src/components/ProfileBanner.js
@@ -5,6 +5,8 @@ import Loading from './Loading'
 
 import { FETCH_USER_QUERY, UPDATE_USER } from "../Calls";
 
+const breaks = [0, 100, 200, 400, 800, 1600, 3200, 6400, 12800, 25600, 51200]
+
 const ProfileBanner = ({ user }) => {
   const { contextUserId } = useContext(AuthContext);
   const [following, setFollowing] = useState()
@@ -37,6 +39,10 @@ const ProfileBanner = ({ user }) => {
   }
 
   if(!user || !visitor) return <Loading/>
+
+  let percentage = user?.level ? ( (user.points-breaks[user.level-1]) / (breaks[user.level]-breaks[user.level-1]) * 100 )   : '0'
+  let start = user?.level ?  breaks[user.level-1]+'' : '0'
+  let end = user?.level ? breaks[user.level]+''  : '100'
   
   return(
     <div className="container-fluid px-0 h-100 pt-5" style={{backgroundColor: user.color}}>
@@ -57,12 +63,30 @@ const ProfileBanner = ({ user }) => {
             </div>
           </div>
 
-          <div className="row mx-0 mt-3 justify-content-center">
-            <div className="col col-auto px-0 pe-2"> 
-              <span className="badge bg-dark" style={{fontSize: '20px'}}>{user?.points} Points</span>
+          <div className="row mx-0 mt-3 pb-1 justify-content-center">
+            <div className="col-auto px-0 pt-4"> 
+              <span className="badge bg-light text-dark" style={{fontSize: '20px'}}>Level {user?.level}</span>
             </div>
-            <div className="col col-auto px-0"> 
-              <span className="badge bg-dark" style={{fontSize: '20px'}}>Level {user?.level}</span>
+          </div>
+          <div className="row mx-0 justify-content-center">
+            <div className="col-auto px-0"> 
+              <span className="badge bg-light text-dark" style={{fontSize: '20px'}}>{user?.points} Points</span>
+            </div>
+          </div>
+
+
+          <div className="row mx-0 justify-content-center px-1">
+            <div className="col-12 pt-2"> 
+              <div className="progress" style={{height: '10px'}}>
+                <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{ width: percentage+'%' }} aria-valuenow={percentage} aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+            <div className="col-auto">
+              <span className="badge bg-light text-success p-1">{start}</span>
+            </div>
+            <div className="col"></div>
+            <div className="col-auto">
+              <span className="badge bg-light text-success p-1">{end}</span>
             </div>
           </div>
 
